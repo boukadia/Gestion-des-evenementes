b965b9a37461a3f8bda3d3b7a1594387e01621b0
boukadia <mustapha.boukadia97@gmail.com>
2026-02-10 21:22:02 +0100

    ...

--- Backend/src/evenementes/evenementes.module.ts
+++ Backend/src/evenementes/evenementes.module.ts
@@ -3,7 +3,6 @@ import { EvenementesService } from './evenementes.service';
 import { EvenementesController } from './evenementes.controller';
 import { PrismaModule } from 'src/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
-
 @Module({
   imports: [PrismaModule],
   controllers: [EvenementesController],
